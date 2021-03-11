7f29a645c4aa1827e3f123a9aada1c59e83b2548
RafaMaths <rmathscoach@gmail.com>
2021-03-11 17:41:22 -0300

    entrega requisitos 6 a 10

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
-// function createButton() {
-//   let button = document.getElementById('criar-tarefa');
-//   button.addEventListener('click', (event) => {
-//     let task = document.getElementById('texto-tarefa').innerText;
-//     let evento = event;
-//   })
-//   console.log("ola");
-// }
-// createButton();
+const task = document.getElementById('texto-tarefa');
+const btnCriaTarefa = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const btnLimpar = document.querySelector('#apaga-tudo');
 
-// function creatOlist(texto) {
-//   let addItem = document.createElement('li');
-//   addItem = texto.innerText;
-//   let getList = document.getElementById('lista-tarefas');
-//   getList.appendChild(addItem);
-// }
-// createOlist("dsadsa");
+function selectLi(event) {
+  const listLi = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < listLi.length; i += 1) {
+    if (listLi[i].classList.contains('selected')) {
+      listLi[i].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
+}
 
+/* pesquisa toggle */
+function mark(event) {
+  event.target.classList.toggle('completed');
+}
 
-let task = document.getElementById('texto-tarefa');        /* input */
-let button = document.getElementById('criar-tarefa');      /* botão */
-let list = document.getElementById('lista-tarefas');      /* lista */
-let clickList = document.querySelectorAll('ol');          /* armazena lista */
-button.addEventListener('click', function(){  
-  let novaLista = document.createElement('li');
-  list.appendChild(novaLista).innerText = task.value;     /* adiciono a li o texto adicionado à task */
-  task.value = '';                                        /* retorna uma string com o conteúdo digitado */
+function criarTarefa() {
+  const novoItem = document.createElement('li');
+  novoItem.className = 'tarefa';
+  novoItem.addEventListener('click', selectLi);
+  novoItem.addEventListener('dblclick', mark);
+  novoItem.innerText = task.value;
+  // if (!(task.value).trim()) {
+  //   return alert('Adicione uma tarefa');
+  // }
+  list.appendChild(novoItem);
+  task.value = '';
+  task.focus();
+}
+
+btnCriaTarefa.addEventListener('click', criarTarefa);
+task.addEventListener('keyup', (event) => {
+  console.log(event.key);
+  if (event.key === 'Enter') {
+    criarTarefa();
+  }
 });
 
-for (let i = 0; i < clickList.length; i++) {
-  clickList[i].addEventListener('click', function() {
-    clickList.target.style.backgroundColor = 'rgb(128, 128, 128)';
+btnLimpar.addEventListener('click', () => {
+  list.innerHTML = '';
+  // let array = document.querySelectorAll('.tarefa');
+  // for (let i = 0; i < array.length; i += 1) {
+  //   list.removeChild(array[i]);
+  // }
 });
-} 
-
-
-
-
-// creatOlist()
-
-// function creatOlist () {
-//   let addItem = document.createElement('li');
-//   let getList = document.getElementById('lista-tarefas');
-//   getList.appendChild(addItem);
-// }
-
-// function createButton() {
-//   let button = document.getElementById('criar-tarefa');
-  
-//   button.addEventListener('click', (event) => {
-//     let task = document.getElementById('texto-tarefa').innerText;
-//     let evento = event;
-//   })
-// }
